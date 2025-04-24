8fcc0dcb12e2718257ff068ab6aabaf7d0b4c738
ZV <wazolee@gmail.com>
2025-04-24 17:20:44 +0200

    end

---
+++ src/app/app-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'saa-app-nav',
+    templateUrl: './app-nav.component.html',
+    styleUrl: './app-nav.component.scss',
+    standalone: false
+})
+
+export class AppNavComponent {
+}
